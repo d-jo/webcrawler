8ac6ecc1e330f7146dd34b818b1951fb2dcf8493
d-jo <declan.l.johnson@gmail.com>
2022-02-23 11:47:18 -0600

    Add test to ensure same domain only

--- usecase/crawler/service_test.go
+++ usecase/crawler/service_test.go
@@ -267,3 +267,81 @@ func TestCrawl3(t *testing.T) {
 		t.Error("expected 0 child pages")
 	}
 }
+
+func TestCrawl4(t *testing.T) {
+
+	var handlerFunc http.HandlerFunc
+
+	var pages map[string]string
+
+	handlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		p, ok := pages[r.URL.Path]
+		if ok {
+			w.Write([]byte(p))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+
+	srv := httptest.NewServer(handlerFunc)
+	defer srv.Close()
+
+	pages = map[string]string{
+		"/":      fmt.Sprintf("<a href=\"%s/page1\">page1</a>", srv.URL),
+		"/page1": fmt.Sprintf("<a href=\"%s/page2\">page2</a><a href=\"localhost:5252/outside\">outside page</a>", srv.URL),
+		"/page2": "empty page wiith no links",
+	}
+
+	memRepo, err := crawler_mem.NewRepoService()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	pageService, err := crawled_page.NewService(memRepo)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	crawler := crawler.NewService(pageService)
+	defer crawler.CloseAllWorkers()
+
+	sc := entity.StartCommand{
+		Url: srv.URL + "/",
+	}
+
+	resp, _ := crawler.StartCrawling(context.TODO(), &sc)
+
+	if resp.Success != true {
+		t.Error("expected success:", resp.Message)
+	}
+
+	lc := entity.ListCommand{}
+
+	time.Sleep(time.Second * 2)
+
+	list, _ := crawler.List(context.TODO(), &lc)
+
+	var buf strings.Builder
+	util.FPrintChildren(&buf, list.Root[0], 0)
+
+	t.Logf("%+v", len(list.Root))
+	t.Log(buf.String())
+
+	if len(list.Root) != 1 {
+		t.Error("expected 1 root page")
+	}
+
+	if len(list.Root[0].Children) != 1 {
+		t.Error("expected 1 child page")
+	}
+
+	if len(list.Root[0].Children[0].Children) != 1 {
+		t.Error("expected 0 child pages")
+	}
+
+	if len(list.Root[0].Children[0].Children[0].Children) != 0 {
+		t.Error("expected 0 child pages")
+	}
+}
